251567579c6a083234360ad76319dbb6ec399ed2
cbth3s <vvtf77@gmail.com>
2025-12-14 13:36:16 +0300

    add API endpoints (localhost)

--- Hotels/Services/Network/APIEndpoint.swift
+++ Hotels/Services/Network/APIEndpoint.swift
@@ -3,6 +3,7 @@ import Foundation
 
 struct APIEndpoint {
     private static let baseDataURL = "https://raw.githubusercontent.com/cbth3s/HotelsApp-Backend/heads/main"
+    private static let baseDataLocalURL = "http://127.0.0.1:8000"
     
     static func hotelList() -> URL? {
         URL(string: baseDataURL + "/hotels.json")
@@ -14,5 +15,19 @@ struct APIEndpoint {
     
     static func hotelImage(name: String) -> URL? {
         URL(string: baseDataURL + name)
+    }
+    
+    // MARK: API endpoints
+    
+    static func localHotelList() -> URL? {
+        URL(string: baseDataLocalURL + "/hotels")
+    }
+    
+    static func localHotelDetails(id: Int) -> URL? {
+        URL(string: baseDataLocalURL + "/hotels/\(id)/details")
+    }
+    
+    static func localLoadImage(path: String) -> URL? {
+        URL(string: baseDataLocalURL + path)
     }
 }
